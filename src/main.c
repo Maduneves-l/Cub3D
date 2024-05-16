9b3b99b703b439db5f79871df274dfbf17e7f027
Maduneves-l <neves.maedu@gmail.com>
2024-05-16 12:47:55 +0100

    done

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mneves-l <mneves-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 14:31:14 by gsilva            #+#    #+#             */
-/*   Updated: 2024/05/16 11:58:02 by mneves-l         ###   ########.fr       */
+/*   Updated: 2024/05/16 12:47:37 by mneves-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,9 +42,11 @@ t_map	*map(void)
 
 int	main(int argc, char **argv)
 {
-	if(ac == 1)
-		error("Missing arguments");
-	(void)argc;
+	if(argc == 1)
+	{
+		error("Missing arguments", 0);
+		return (1);
+	}
 	if (!check_ext(argv[1]))
 	{
 		error("Invalid map extension", 0);
